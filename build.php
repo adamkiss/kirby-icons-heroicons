829085df924d882f360920275c46a8b5cdfadee2
Adam Kiss <iam@adamkiss.com>
2022-05-25 19:40:48 +0200

    Fix build script

--- build.php
+++ build.php
@@ -6,13 +6,10 @@
 	modifies them to add the check for attributes and saves them as PHP files
 */
 
-$solid = new DirectoryIterator(__DIR__ . '/heroicons/optimized/solid');
-$outline = new DirectoryIterator(__DIR__ . '/heroicons/optimized/outline');
-
 mkdir(__DIR__ . '/snippets/solid', recursive: true);
 mkdir(__DIR__ . '/snippets/outline', recursive: true);
 
-foreach ($solid as $file) {
+foreach (new DirectoryIterator(__DIR__ . '/heroicons/optimized/solid') as $file) {
 	if ($file->getType() !== 'file') {
 		continue;
 	}
@@ -23,7 +20,7 @@ foreach ($solid as $file) {
 	file_put_contents($name, $icon);
 }
 
-foreach ($outline as $file) {
+foreach (new DirectoryIterator(__DIR__ . '/heroicons/optimized/outline') as $file) {
 	if ($file->getType() !== 'file') {
 		continue;
 	}
@@ -31,7 +28,7 @@ foreach ($outline as $file) {
 	$icon = file_get_contents($file->getPathname());
 	$icon = str_replace('<svg', '<svg class="<?= $class ?? \'\' ?>"', $icon);
 	$icon = str_replace('stroke-width="2"', 'stroke-width="<?= $strokeWidth ?? 2 ?>"', $icon);
-	$name = __DIR__ . '/snippets/solid/' . str_replace('svg', 'php', $file->getFilename());
+	$name = __DIR__ . '/snippets/outline/' . str_replace('svg', 'php', $file->getFilename());
 	file_put_contents($name, $icon);
 }
 
