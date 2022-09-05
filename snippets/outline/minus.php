9f7ee94e0ca223743947d66c08ab21e9653b817f
Adam Kiss <iam@adamkiss.com>
2022-09-05 18:13:47 +0200

    Run the build and replace heroicons 1 with heroicons 2

--- snippets/outline/minus.php
+++ snippets/outline/minus.php
@@ -1,3 +1,3 @@
-<svg class="<?= $class ?? '' ?>" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="<?= $strokeWidth ?? 2 ?>" stroke="currentColor" aria-hidden="true">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M20 12H4"/>
+<svg class="<?= $class ?? '' ?>" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="<?= $strokeWidth ?? 1.5 ?>" stroke="currentColor" aria-hidden="true">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 12h-15"/>
 </svg>
